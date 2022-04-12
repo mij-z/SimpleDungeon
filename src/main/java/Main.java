f33aaa3e2b70e4ec5f6bc2c237de7ec722962d0a
mjz <mjz@sfu.ca>
2022-04-12 02:48:07 -0700

    initializes the creation of the JFrame

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import gui.Window;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int width = 1024;
+        int height = width / 12 * 9;
+
+        new Window(width, height, "Simple Dungeon");
+    }
+
+}
