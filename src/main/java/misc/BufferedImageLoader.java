3d943020384bc73ddda23f20ec8d89a59364bbc6
mjz <mjz@sfu.ca>
2022-04-13 23:25:52 -0700

    implemented class to handle image loading

---
+++ src/main/java/misc/BufferedImageLoader.java
@@ -0,0 +1,14 @@
+package misc;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Objects;
+
+public class BufferedImageLoader {
+
+    public BufferedImage loadImage(String path) throws IOException {
+        return ImageIO.read(Objects.requireNonNull(getClass().getResource(path)));
+    }
+
+}
